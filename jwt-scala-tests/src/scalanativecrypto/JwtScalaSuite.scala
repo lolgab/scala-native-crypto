47023cac830a9921670d9b003152b06192164791
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-11-01 18:40:23 +0100

    Use actual values in test

--- jwt-scala-tests/src/scalanativecrypto/JwtScalaSuite.scala
+++ jwt-scala-tests/src/scalanativecrypto/JwtScalaSuite.scala
@@ -11,24 +11,31 @@ object JwtExampleSuite extends TestSuite {
     test("JWT should be correctly generated, signed, and verified") {
       // Define a secret key for signing the JWT
       val secretKey = "your-secret-key"
+
+      val claimContent = """{"user":"exampleUser","role":"admin"}"""
+      val expiration =
+        Some(7284447535L) // We will be all dead when this expires
       // Create a claim (payload) for the JWT
       val claim = JwtClaim(
-        content = """{"user":"exampleUser","role":"admin"}""",
-        expiration = Some(Instant.now.plusSeconds(3600).getEpochSecond)
+        content = claimContent,
+        expiration = expiration
       )
 
       // Encode (sign) the JWT using HMAC SHA-256
       val token = JwtUpickle.encode(claim, secretKey, JwtAlgorithm.HS256)
-      assert(token.nonEmpty)
+      assert(
+        token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjcyODQ0NDc1MzUsInVzZXIiOiJleGFtcGxlVXNlciIsInJvbGUiOiJhZG1pbiJ9.nGh3SFGmePsMPl4J9Jg5qFQ6FkVss6M5lh7QXwTB-44"
+      )
 
       // Decode and verify the token
       JwtUpickle.decode(token, secretKey, Seq(JwtAlgorithm.HS256)) match {
         case Success(decodedClaim) =>
-
-          assert(decodedClaim.content == claim.content)   // Check payload
-          assert(decodedClaim.expiration == claim.expiration) // Check expiration
+          assert(decodedClaim.content == claimContent) // Check payload
+          assert(decodedClaim.expiration == expiration) // Check expiration
         case Failure(exception) =>
-          sys.error(s"JWT verification failed with error: ${exception.getMessage}")
+          sys.error(
+            s"JWT verification failed with error: ${exception.getMessage}"
+          )
       }
     }
   }
