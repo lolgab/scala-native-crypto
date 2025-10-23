08bd032afae7eb819a022293797d851891f366a7
Lanqing Huang <lqhuang@outlook.com>
2025-10-23 21:21:44 +0800

    test: Improve how `CryptoPrimitive` simulate Java enums and add unit test

--- scala-native-crypto/src/java/security/CryptoPrimitive.scala
+++ scala-native-crypto/src/java/security/CryptoPrimitive.scala
@@ -1,39 +1,47 @@
 package java.security
 
+import java.lang._Enum
+
 /// ## Refs
 ///
 /// https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/security/CryptoPrimitive.html
-sealed abstract class CryptoPrimitive
+sealed class CryptoPrimitive private (name: String, ordinal: Int)
+    extends _Enum[CryptoPrimitive](name, ordinal)
 object CryptoPrimitive {
 
-  /** Symmetric primitive: block cipher */
-  case object BLOCK_CIPHER extends CryptoPrimitive
+  final val BLOCK_CIPHER = new CryptoPrimitive("BLOCK_CIPHER", 0)
+  final val KEY_AGREEMENT = new CryptoPrimitive("KEY_AGREEMENT", 1)
+  final val KEY_ENCAPSULATION = new CryptoPrimitive("KEY_ENCAPSULATION", 2)
+  final val KEY_WRAP = new CryptoPrimitive("KEY_WRAP", 3)
+  final val MAC = new CryptoPrimitive("MAC", 4)
+  final val MESSAGE_DIGEST = new CryptoPrimitive("MESSAGE_DIGEST", 5)
+  final val PUBLIC_KEY_ENCRYPTION =
+    new CryptoPrimitive("PUBLIC_KEY_ENCRYPTION", 6)
+  final val SECURE_RANDOM = new CryptoPrimitive("SECURE_RANDOM", 7)
+  final val SIGNATURE = new CryptoPrimitive("SIGNATURE", 8)
+  final val STREAM_CIPHER = new CryptoPrimitive("STREAM_CIPHER", 9)
 
-  /** Asymmetric primitive: key agreement and key distribution */
-  case object KEY_AGREEMENT extends CryptoPrimitive
+  val _values = Array(
+    BLOCK_CIPHER,
+    KEY_AGREEMENT,
+    KEY_ENCAPSULATION,
+    KEY_WRAP,
+    MAC,
+    MESSAGE_DIGEST,
+    PUBLIC_KEY_ENCRYPTION,
+    SECURE_RANDOM,
+    SIGNATURE,
+    STREAM_CIPHER
+  )
 
-  /** Asymmetric primitive: key encapsulation mechanism */
-  case object KEY_ENCAPSULATION extends CryptoPrimitive
-
-  /** Symmetric primitive: key wrap */
-  case object KEY_WRAP extends CryptoPrimitive
-
-  /** Symmetric primitive: message authentication code */
-  case object MAC extends CryptoPrimitive
-
-  /** Hash function */
-  case object MESSAGE_DIGEST extends CryptoPrimitive
-
-  /** Asymmetric primitive: public key encryption */
-  case object PUBLIC_KEY_ENCRYPTION extends CryptoPrimitive
-
-  /** Cryptographic random number generator */
-  case object SECURE_RANDOM extends CryptoPrimitive
-
-  /** Asymmetric primitive: signature scheme */
-  case object SIGNATURE extends CryptoPrimitive
-
-  /** Symmetric primitive: stream cipher */
-  case object STREAM_CIPHER extends CryptoPrimitive
-
+  def values(): Array[CryptoPrimitive] = _values.clone()
+  def valueOf(name: String): CryptoPrimitive = {
+    _values
+      .find(_.name() == name)
+      .getOrElse({
+        throw new IllegalArgumentException(
+          "No enum const CryptoPrimitive." + name
+        )
+      })
+  }
 }
